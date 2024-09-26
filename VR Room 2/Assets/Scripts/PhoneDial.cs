ab8538e1b9291210892bee033a43cd5654cc472a
SeanBos03 <142151673+SeanBos03@users.noreply.github.com>
2024-09-26 13:19:38 +0200

    what I added

--- VR Room 2/Assets/Scripts/PhoneDial.cs
+++ VR Room 2/Assets/Scripts/PhoneDial.cs
@@ -7,8 +7,15 @@ using TMPro;
 public class PhoneDial : MonoBehaviour
 {
     [SerializeField] TMP_Text theText;
+    [SerializeField] AudioClip theCallButtonSound;
+    [SerializeField] GameObject audioSourceObject;
+    AudioSource playerAudioSource;
     string theNumber = "";
 
+    void Start()
+    {
+        playerAudioSource = audioSourceObject.GetComponent<AudioSource>();
+    }
     public string TheNumber
     {
         get { return theNumber; }
@@ -25,7 +32,8 @@ public class PhoneDial : MonoBehaviour
         {
             this.theNumber = "";
         }
-
+        playerAudioSource.clip = theCallButtonSound;
+        playerAudioSource.Play();
         this.theNumber = this.theNumber + theNumber;
         UpdateText();
     }
